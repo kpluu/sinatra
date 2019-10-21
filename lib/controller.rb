9868afd6d0f9cf1b9e2eb9cbb8c41d9e657ea0b4
KPLUU <kph.luu@gmail.com>
2019-10-21 17:05:18 +0200

    first commit

---
+++ lib/controller.rb
@@ -0,0 +1,24 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save
+    redirect '/'
+  end
+  
+  get '/gossips/:id' do
+    puts "*******************"
+    puts "ceci est mon ID : #{params[:id]}"
+  end
+  
+
+end
